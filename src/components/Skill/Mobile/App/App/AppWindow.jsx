c67ebd927c4038aa09cf16dff59d4f2f03b9b4bd
Matheus Pontes <matheuspontes5@hotmail.com>
2025-05-08 16:12:24 -0300

    ajuste margem botoes mobile

--- src/components/Skill/Mobile/App/App/AppWindow.jsx
+++ src/components/Skill/Mobile/App/App/AppWindow.jsx
@@ -7,17 +7,19 @@ const AppWindow = ({ nomeDoApp, aoClicarEmVoltar }) => {
     return (
         <div className="bg-[url(/images/bgapp.png)] bg-cover h-[597px] flex flex-col items-center justify-center">
             <h1 className="absolute top-[177px] ml-[20px] text-white opacity-80"> {nomeDoApp} </h1>
-            <div className="absolute top-[250px] mr-[85px] ml-[100px]  grid grid-cols-3 place-items-center">
-                {skills[nomeDoApp].map((skill) =>
+            <div className="flex flex-col mt-[130px] gap-[215px]">
+                <div className="pt-[0px] mr-[85px] ml-[100px]  grid grid-cols-3 place-items-center">
+                    {skills[nomeDoApp].map((skill) =>
 
-                    <AppSkillsIcon
-                        key={skill.id}
-                        nome={skill.nome}
-                        tipo={skill.tipo}
-                    />
-                )}
+                        <AppSkillsIcon
+                            key={skill.id}
+                            nome={skill.nome}
+                            tipo={skill.tipo}
+                        />
+                    )}
+                </div>
+                <MobileButtons aoClicarEmVoltar={aoClicarEmVoltar} color="B" posicao="ml-[20px]" />
             </div>
-            <MobileButtons aoClicarEmVoltar={aoClicarEmVoltar} color="B" posicao="mt-[480px] ml-[20px]"/>
         </div>
     )
 }
--- src/components/Skill/Mobile/App/Main/MainMobile.jsx
+++ src/components/Skill/Mobile/App/Main/MainMobile.jsx
@@ -4,13 +4,13 @@ import AppIcons from "../AppIcons"
 
 const MainMobile = ({ AoClicar, aoClicarEmVoltar }) => {
     return (
-        <div className="bg-[url(/images/bgmob.webp)] bg-cover h-[597px] flex flex-col items-center">
-            <div className="relative flex rounded-[20px] gap-[9px] mt-[130px] mr-[20px]">
+        <div className="bg-[url(/images/bgmob.webp)] bg-cover h-[597px] flex flex-col items-center gap-[330px]">
+            <div className="relative flex rounded-[20px] gap-[9px] mt-[145px] mr-[20px]">
                 <AppIcons AoClicarNoApp={AoClicar} icone="F" nome="Front-end" className="bg-azulEscuro text-brancoCinza" />
                 <AppIcons AoClicarNoApp={AoClicar} icone="B" nome="Back-end" className="bg-brancoCinza text-black" />
 
             </div>
-            <MobileButtons aoClicarEmVoltar={aoClicarEmVoltar} color="W" posicao="mt-[345px] ml-[20px]"/>
+            <MobileButtons aoClicarEmVoltar={aoClicarEmVoltar} color="W" posicao="ml-[20px]"/>
         </div>
     )
 }
