54911954f3f098f434eeaab9c55f90b0975de6d4
Matheus Pontes <matheuspontes5@hotmail.com>
2025-05-08 17:34:15 -0300

    ajuste footer do componente contactPage

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -3,7 +3,8 @@ import Footer from "@/components/Footer/Footer"
 
 const ContactPage = () => {
     return (
-        <div className="flex flex-col items-center xl:mt-[20px] justify-center gap-[20px] xl:pt-[0] ">
+        <>
+        <div className="flex flex-col items-center xl:mt-[20px] pt-[50px] justify-center gap-[20px]">
             <div className="flex justify-center items-center bg-[url(/images/comentarios.png)] bg-cover 
             xl:w-[500px] xl:h-[440px] w-[320px] h-[340px] xl:mr-0 mr-[50px] pt-[20px] ">
                 <p className="xl:text-[24px] text-[18px] xl:pb-0 pb-[40px] text-center font-bold">
@@ -17,8 +18,9 @@ const ContactPage = () => {
                 <MiniCard nome="Linkedin" url="https://www.linkedin.com/in/matheus-pontes-2b352b2bb/" />
                 <MiniCard nome="Github" url="https://github.com/MathPontes12" />
             </section>
-            <Footer posicao="top-[70px]"/>
         </div>
+        <Footer posicao="top-[180px]"/>
+        </>
     )
 }
 
